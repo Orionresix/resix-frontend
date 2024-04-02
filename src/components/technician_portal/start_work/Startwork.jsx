245d555621086f3979325c22cf5585b8a4c72461
KarenWanyama3430 <karenwanyama3430@gmail.com>
2024-04-02 18:09:28 +0300

    changes to the technician-portal

--- src/components/technician_portal/start_work/Startwork.jsx
+++ src/components/technician_portal/start_work/Startwork.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import '../Startwork.css'
 const Startwork = () => {
   return (
     <div>
-      <div> 
+      <div className='Welcome'> 
       <img src={logo} alt="logo"  />
         <h1>Work Order</h1>
         <h2>Ticket No</h2>
         <img src={request} alt="request"  />
         </div>
 
-        <div> 
+        <div className='description'> 
         <p>Issue:Bathroom Sink Leak</p>
         <p>Type:Plumbing</p>
         <p>Description:Significant leak observed originatinating from the juction
@@ -21,6 +21,10 @@ const Startwork = () => {
 
         <p>Potential Cause:Degraded seal or crack in the piping</p>
         </div>
+
+        <Button type='submit' className='startwork_button' variant='contained'>
+            Start Work
+          </Button>
       
     </div>
   )
